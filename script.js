391af6ff931abdde988ffaec96560c63a38fb0c4
Iago Breno Alves do Carmo Araujo <ibaraujo@poshul.ime.usp.br>
2018-07-26 18:56:51 -0300

    Add user story 7

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     quotes = ["Success is the ability to go from failure to failure without lossing enthusiasm"];
+    authors = ["Johnny Depp"];
     $("#text").append(quotes[0]);
+    $("#author").append(authors[0]);
 });

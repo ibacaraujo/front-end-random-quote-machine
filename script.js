8eb522d3348c19ad3fe19973495ada670f6c387e
Iago Breno Alves do Carmo Araujo <ibaraujo@viper.ime.usp.br>
2018-07-28 15:49:57 -0300

    Add user story 8

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
-    quotes = ["Success is the ability to go from failure to failure without lossing enthusiasm"];
-    authors = ["Johnny Depp"];
-    $("#text").append(quotes[0]);
-    $("#author").append(authors[0]);
+  quotes = [
+    "Success is the ability to go from failure to failure without loosing enthusiasm",
+    "Only I can change my life. No one can do it for me",
+    "Good, better, best. Never let it rest. 'Til your good is better and your better is best",
+    "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence"
+    ];
+    
+  authors = [
+    "Johnny Depp",
+    "Carol Burnett",
+    "St. Jerome",
+    "Helen Keller"
+    ];
+  
+  $("#text").append(quotes[0]);
+  $("#author").append(authors[0]);
+    
+  $("#new-quote").click(function() {
+    getNewQuote(quotes);
+  });
+  
 });
+
+function getNewQuote(quotes) {
+  index = Math.floor(Math.random() * quotes.length);
+  quote = quotes[index];
+  author = authors[index];
+  $("#text").html(quote);
+  $("#author").html(author);
+}

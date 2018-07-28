3bcd0a367923615f9dd1932468b37e9148a51e4b
Iago Breno Alves do Carmo Araujo <ibaraujo@viper.ime.usp.br>
2018-07-28 15:51:50 -0300

    Add user story 9 too

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ $(document).ready(function() {
     "Success is the ability to go from failure to failure without loosing enthusiasm",
     "Only I can change my life. No one can do it for me",
     "Good, better, best. Never let it rest. 'Til your good is better and your better is best",
-    "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence"
+    "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence",
+    "With the new day comes new strength and new thoughts"
     ];
     
   authors = [
     "Johnny Depp",
     "Carol Burnett",
     "St. Jerome",
-    "Helen Keller"
+    "Helen Keller",
+    "Eleanor Roosevelt"
     ];
   
   $("#text").append(quotes[0]);

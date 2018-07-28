28913ff207d4f2ec93cf68e0fbd1225a84d042d6
Iago Breno Alves do Carmo Araujo <ibaraujo@viper.ime.usp.br>
2018-07-28 16:24:43 -0300

    Update style

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
     "Only I can change my life. No one can do it for me",
     "Good, better, best. Never let it rest. 'Til your good is better and your better is best",
     "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence",
-    "With the new day comes new strength and new thoughts"
+    "With the new day comes new strength and new thoughts",
+    "Failure will never overtake me if my determination to succeed is strong enough",
+    "Change your life today. Don't gamble on the future, act now, without delay",
+    "It always seems impossible until it's done",
+    "We should not give up and we should not allow the problem to defeat us",
+    "It does not matter how slowly you go as long as you do not stop",
+    "Start where you are. Use what you have. Do what you can"
     ];
     
   authors = [
@@ -12,7 +18,13 @@ $(document).ready(function() {
     "Carol Burnett",
     "St. Jerome",
     "Helen Keller",
-    "Eleanor Roosevelt"
+    "Eleanor Roosevelt",
+    "Og Mandino",
+    "Simone de Beauvoir",
+    "Nelson Mandela",
+    "A. P. J. Abdul Kalam",
+    "Confucius",
+    "Arthur Ashe"
     ];
   
   $("#text").append(quotes[0]);
